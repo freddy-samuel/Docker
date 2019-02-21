6820aa19129e5bd23746c5e39e67f10e4ddfbf83
Freddy <freddyjohnsamuel@gmail.com>
2019-02-21 02:29:37 -0800

    docker properties

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
-ADD target/ekyc-0.0.1-SNAPSHOT.jar KYCApp.jar
+ADD target/ekyc-docker-0.0.1-SNAPSHOT.jar KYCApp.jar
 ENV JAVA_OPTS=""
 ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /KYCApp.jar" ]
--- src/main/java/com/boa/kyc/docker/DockerApplication.java
+++ src/main/java/com/boa/kyc/docker/DockerApplication.java
@@ -2,8 +2,16 @@ package com.boa.kyc.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.bofa.kyc.*")
+@EnableJpaRepositories(basePackages="com.bofa.kyc.*")
+@EntityScan(basePackages="com.bofa.kyc.*")
+@EnableMongoRepositories(basePackages="com.bofa.kyc.*")
 public class DockerApplication {
 
 	public static void main(String[] args) {
